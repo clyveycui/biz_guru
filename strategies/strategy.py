e0a89bc42cfc030c90f6be385d6b234fc3c1e6c3
cui langyuan <cuilangyuan@hotmail.com>
2025-03-13 16:15:34 +0800

    add single agent approach

--- strategies/strategy.py
+++ strategies/strategy.py
@@ -3,9 +3,5 @@ from abc import ABC, abstractmethod
 # Just a template, you can either inherit or build something similar to this
 class Strategy(ABC):
     @abstractmethod
-    def init_agent_workers(self):
-        pass
-
-    @abstractmethod
     def execute_strategy(self):
         pass
--- strategies/strategy_vanilla.py
+++ strategies/strategy_vanilla.py
@@ -1,46 +1,40 @@
 from .strategy import Strategy
 from agents import AgentLlama3
+from ..data.get10KSections import SEC10KExtractor
 import json
 
-# The strategy mimics a one supervisor many expert setting.
-# One agent acting as the supervisor makes the final decision
-# Multiple agents acting as experts provide advice (expert opinion) to the supervisor regarding specific areas they are experts in
-# All info/data is distributed to all agents
 class StrategyVanilla(Strategy):
-    INSTRUCTION_PATH = "prompts/strategy_vanilla_instructions.json"
-    SUPERVISOR_PROMPT_FORMAT = "{advices}\n{question}"
-    def __init__(self, data):
-        self.data = data
-        # self.experts = {}
-        self.supervisor = None
+    INSTRUCTION_PATH = "prompts/strategy_vanilla.json"
+    CATEGORY_PATH = "prompts/categories.json"
+    def __init__(self, ticker = 'NVDA'):
+        self.agent = AgentLlama3()
         with open(StrategyVanilla.INSTRUCTION_PATH, "r") as f:
-            self.instructions = json.load(f)
+            self.prompts = json.load(f)
+        with open(StrategyVanilla.CATEGORY_PATH, "r") as f:
+            self.categories = json.load(f)
+        self.doc_retriever = SEC10KExtractor()
+        self.ticker = ticker
 
-    #use 1 model to simulate everyone for now
-    def init_agent_workers(self):
-        supervisor_instruction = self.instructions['supervisor'].format(report = self.data['report'])
-        # risk_expert_instruction = instructions['risk_expert'].format(report = self.data['report'])
-        # biz_expert_instruction = instructions['biz_expert'].format(report = self.data['report'])
-
-        self.supervisor = AgentLlama3()
-        # self.experts['risk'] = AgentLlama3(sys_instruction=risk_expert_instruction)
-        # self.experts['business'] = AgentLlama3(sys_instruction=biz_expert_instruction)
+    # Step 1
+    def _get_necessary_documents(self, user_query):
+        USER_PROMPT = self.prompts['get_necessary_documents'].format(user_query=user_query, categories=str(self.categories))
+        return self.agent.ask(USER_PROMPT).split("<begin of answer>")[1].split("<end of answer>")[0].replace("\n", "").strip()
     
-    #TODO
-    def agent_model_worker(self):
-        pass
+    # Step 2
+    def _get_doc_details(self, list_docs):
+        details_dict = self.doc_retriever.getSectionsByName(ticker=self.ticker, sections=list_docs)
+        return details_dict
 
-    def execute_strategy(self, question):
-        advices = []
-        for agent_name, sys_instruction in self.instructions.items():
-            if agent_name == "supervisor":
-                continue
-            advice = self.supervisor.ask(sys_instruction, question)
-            s = f"{agent_name} expert: {advice}"
-            print(s)
-            advices.append(s)
-        supervisor_prompt = StrategyVanilla.SUPERVISOR_PROMPT_FORMAT.format(advices="\n".join(advices), question=question)
-        response = self.supervisor.ask(self.instructions["supervisor"], supervisor_prompt)
-        return response
+    # Step 3
+    def execute_strategy(self, user_query):
+        try: list_docs = eval(self._get_necessary_documents(user_query))
+        except: list_docs = ["Business"]
+    
+        docs_details = """"""
+        details_dict = self._get_doc_details(list_docs)
+        for doc_id in list_docs: docs_details += doc_id +"\n\n" + details_dict[doc_id] + "\n-----------------------------"
+    
+        GET_ANSWER_PROMPT = self.prompts['get_answer'].format(docs_details=docs_details, user_query=user_query)
+        return self.agent.ask(GET_ANSWER_PROMPT)
 
         
